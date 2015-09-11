2b6692eaca7164e6b44cde410255a19f1805a53c
Šime Jerković <sime@sistemi.hr>
2015-09-11 22:42:16 +0200

    added execution policy bypass

--- 01_RunAsAdminitrator.bat
+++ 01_RunAsAdminitrator.bat
@@ -5,5 +5,5 @@ REM -ExecutionPolicy ByPass -File %~dp001_CreateWebsite_Dev.ps1
 
 powershell.exe -Command "& { dir *.ps1 | Unblock-File }"
 powershell.exe -Command "& { Set-ExecutionPolicy -Scope CurrentUser -ExecutionPolicy RemoteSigned }"
-powershell.exe -File .\install.ps1
+powershell.exe -ExecutionPolicy ByPass -File .\install.ps1
 pause
