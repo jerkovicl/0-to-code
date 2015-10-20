94dd102c990f2a89002edb7210c75e6bbbd17934
Luka Jerković <luka.jerkovic@sistemi.hr>
2015-10-20 12:43:22 +0200

    Update settings.psm1

--- settings.psm1
+++ settings.psm1
@@ -1,12 +1,12 @@
 # Location for chocolatey and npm repositories.
-$global:root = $env:USERPROFILE
+$global:root = $env:ALLUSERSPROFILE
 
 # Known locations.
 $global:chocolateyRoot = "$root\.chocolatey"
 $global:chocolateyToolsRoot = "$root\.chocolatey\apps"
 $global:npmCache = "$root\.npm"
 $global:npmRepository = "$root\.npmrepo"
-$global:dnxRoot = "$env:USERPROFILE\.dnx"
+$global:dnxRoot = "$env:ALLUSERSPROFILE\.dnx"
 
 # Set of NPM modules installed globally by default.
 $global:defaultNpmModules = @("typescript", "yo", "bower", "gulp", "gulp-watch", "gulp-typescript", "gulp-cli", "tsd", "jshint", "grunt-cli", "generator-aspnet")
